a4ece07f6396c9226af982fbdd9b7fe38d34bf7f
Jaswinder Singh <jaswinderdev26@gmail.com>
2024-06-01 12:49:54 +0530

    get services for frontend : get/fontend

--- routers/serviceRouter.js
+++ routers/serviceRouter.js
@@ -10,6 +10,7 @@ import {
 const ServiceRouter = express.Router();
 
 ServiceRouter.get("/get", isAuthenticated, isAuthorized, getAll);
+ServiceRouter.get("/get/fontend", getAll);
 ServiceRouter.post("/get/single/:id", isAuthenticated, isAuthorized, getSingle);
 ServiceRouter.post("/add/service", isAuthenticated, isAuthorized, addService);
 ServiceRouter.put("/update/:id", isAuthenticated, isAuthorized, updateService);
