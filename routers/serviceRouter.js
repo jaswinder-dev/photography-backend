63da8a71bd36b9b76efb53468eb5b8ca8ad561f3
Jaswinder Singh <jaswinderdev26@gmail.com>
2024-06-01 13:02:24 +0530

    error correction in 'get/fontend' as 'get/frontend'

--- routers/serviceRouter.js
+++ routers/serviceRouter.js
@@ -10,7 +10,7 @@ import {
 const ServiceRouter = express.Router();
 
 ServiceRouter.get("/get", isAuthenticated, isAuthorized, getAll);
-ServiceRouter.get("/get/fontend", getAll);
+ServiceRouter.get("/get/frontend", getAll);
 ServiceRouter.post("/get/single/:id", isAuthenticated, isAuthorized, getSingle);
 ServiceRouter.post("/add/service", isAuthenticated, isAuthorized, addService);
 ServiceRouter.put("/update/:id", isAuthenticated, isAuthorized, updateService);
